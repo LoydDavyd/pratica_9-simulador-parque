e96d8713f906ca1ce5b2306e54a11b2523b87042
Loyd Davyd <comercial@loyddavyd.com.br>
2025-06-09 09:42:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+class Parquimetro {
+    constructor(valor){
+        this.valor = parseFloat(valor);
+        this.tempo = 0;
+        this.troco = 0;
+    }
+
+    calcularTempo(){
+        if(this.valor < 1) {
+            return 'Valor insufciente.';
+        }
+    
+
+    if (this.valor >= 2) {
+        this.tempo = 60;
+        this.troco = this.valor - 2;
+    } else if (this.valor >= 1.5) {
+        this.tempo = 45;
+        this.troco = 0;
+    } else if (this.valor >= 1) {
+        this.tempo = 30;
+        this.troco = 0;
+    }
+
+    return `tempo concedido: ${this.tempo} minutos.<br>${this.troco > 0 ? 'Troco: '  + this.troco.toFixed(2) : ''} `;
+
+    }
+}
+
+function calcularEstacinamento() {
+    const valorInserido = document.getElementById('valor').value;
+    const resultadoDiv = document.getElementById('resultado');
+
+    const parquimentro = new Parquimetro(valorInserido);
+    const resultado = parquimentro.calcularTempo();
+
+    resultadoDiv.innerHTML = `<p>${resultado}</p>`;
+}
