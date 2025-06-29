72ea6f740396ce54c6b14cee543eb4db689adf9b
Loyd david <loyddavyd@gmail.com>
2025-06-29 18:48:20 +0100

    Correcao do projecto

--- script.js
+++ script.js
@@ -12,14 +12,15 @@ class Parquimetro {
     
 
     if (this.valor >= 2) {
-        this.tempo = 60;
-        this.troco = this.valor - 2;
+        const blocos = Math.floor(this.valor / 2);
+        this.tempo = blocos * 60;
+        this.troco = (this.valor - blocos * 2);
     } else if (this.valor >= 1.5) {
         this.tempo = 45;
-        this.troco = 0;
+        this.troco = this.valor - 1.5;
     } else if (this.valor >= 1) {
         this.tempo = 30;
-        this.troco = 0;
+        this.troco = this.valor - 0;
     }
 
     return `tempo concedido: ${this.tempo} minutos.<br>${this.troco > 0 ? 'Troco: '  + this.troco.toFixed(2) : ''} `;
